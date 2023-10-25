1c5b3ffc998ef79515180625a0efeb08811ed7a5
JoaoCunha53 <rafacunha53@gmail.com>
2023-10-25 19:36:13 +0100

    Versão 1.0.3

---
+++ SerializacaoCSV/SerializacaoCSV/Pessoa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper.Configuration.Attributes;
+
+namespace SerializacaoCSV
+{
+    internal class Pessoa
+    {
+        [Name("nome")]
+        public string Nome { get; set; }
+
+        [Name("idade")]
+        public int Idade { get; set; }
+
+        //Construtor
+        public Pessoa(string nome, int idade) 
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+
+    }
+}
---
+++ SinalizadorJSON/SinalizadorJSON/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+
+namespace SinalizadorJSON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pessoa> records = new List<Pessoa>();
+
+            //add objetos à lista 
+            records.Add(new Pessoa("José", 28));
+            records.Add(new Pessoa("Maria", 30));
+
+            //Serialização para JSON
+            string json = JsonSerializer.Serialize(records);
+
+            File.WriteAllText("data.json",json);
+
+            //Desserialização de JSON
+            string loadedJson = File.ReadAllText("data.json");
+            var loadedRecords = JsonSerializer.Deserialize<List<Pessoa>>(loadedJson);
+
+            foreach (var record in loadedRecords) 
+            {
+                Console.WriteLine($"Name:{record.Nome} - Age: {record.Idade} ");
+            }
+
+
+
+        }
+    }
+}
