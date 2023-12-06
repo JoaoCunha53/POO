2963ca948c8b695e8ac2b032ebd9f960c4749cea
JoaoCunha53 <rafacunha53@gmail.com>
2023-12-06 13:22:03 +0000

    Update

---
+++ FichaRevisoes/Espetaculos/Espetaculo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Espetaculos
+{
+    internal class Espetaculo
+    {
+
+        //Atributo
+        private string sala { get; set; }
+        private DateOnly data { get; set;}
+        private TimeOnly time { get; set; }
+        private int lugares { get; set; }
+        private int lugaresDisponiveis { get; set; }
+
+        //Construtor
+        public Espetaculo() { }
+        public Espetaculo(string sala, DateOnly data, TimeOnly time, int lugares, int lugaresDisponiveis) 
+        {
+            this.sala = sala;
+            this.data = data;
+            this.time = time;
+            this.lugares = lugares;
+            this.lugaresDisponiveis = lugaresDisponiveis;
+        }
+
+        //Metodos
+        public Espetaculo RegistarEspetaculo()
+        {
+            Console.WriteLine("Identificação da Sala:");
+            string sala = Console.ReadLine();
+            Console.WriteLine("Qual é a Data a agendar (dd-MM-yyyy): ");
+            DateOnly data = DateOnly.Parse(Console.ReadLine());
+            Console.WriteLine("Qual é a Hora (00:00): ");
+            TimeOnly time = TimeOnly.Parse(Console.ReadLine());
+            Console.WriteLine("Lotação maxima: ");
+            int lugares = int.Parse(Console.ReadLine());
+            Espetaculo esp = new Espetaculo(sala, data, time, lugares, lugares);
+            return esp;
+        }
+
+        public void ListarEspetaculo(Espetaculo esp)
+        {
+            Console.WriteLine("Sala: {0}",esp.sala);
+            Console.WriteLine("Data: {0}", esp.data);
+            Console.WriteLine("Hora: {0}", esp.time);
+            Console.WriteLine("Lotação maxima: {0}", esp.lugares);
+            Console.WriteLine("Quantos lugares Disponiveis: {0}", esp.lugaresDisponiveis);
+        }
+
+
+
+
+    }
+}
---
+++ FichaRevisoes/Espetaculos/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Espetaculos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Espetaculo espetaculo = new Espetaculo();
+            List<Espetaculo> listEspetaculo = new List<Espetaculo>();
+            bool inOut = true;
+
+            while (inOut) 
+            {
+                Console.WriteLine("1 - Registar Espetaculos");
+                Console.WriteLine("2 - Consultar Espetaculos");
+                Console.WriteLine("0 - Sair");
+                int op = int.Parse(Console.ReadLine());
+
+                switch(op)
+                {
+                    case 0:
+                        inOut = false; 
+                        break;
+                    case 1:
+                        listEspetaculo.Add(espetaculo.RegistarEspetaculo());
+                        break; 
+                    case 2:
+                        foreach(Espetaculo esp in listEspetaculo) 
+                        {
+                            espetaculo.ListarEspetaculo(esp);
+                        }
+                        break;
+                }
+            }
+
+        }
+    }
+}
